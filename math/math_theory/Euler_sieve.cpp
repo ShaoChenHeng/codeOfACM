856d025282091d42f5e7e1ea0fadc4cc70c5c563
shaochenheng <1010716589@qq.com>
2019-11-07 21:31:18 +0800

    Euler sieve method

---
+++ math/math_theory/Euler_sieve.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100000;
+
+int vis[N], prim[N], xu[N], cnt,n;
+void work(int n)
+{
+    for (int i = 2; i <= n; i++)
+    {
+        if ( !vis[i] ) prim[++cnt] = i;
+        for ( int j = 1; j <= cnt; j++ )
+        {
+            if ( i * prim[j] > n ) break;
+            vis[i*prim[j]] = 1;
+            xu[i*prim[j]] = i;
+            if( i % prim[j] == 0 )break;
+        }
+    }
+}
+
+int main()
+{
+    cnt = 0;
+    scanf("%d",&n);
+    work(n);
+	for(int i = 0; i < n; i++)
+	{
+		printf("%d ", prim[i]);
+	}
+
+}
