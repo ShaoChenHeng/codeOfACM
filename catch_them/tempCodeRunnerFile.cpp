26b53cf2b8d28dcaa69ab6bb5de5d46d1dad0922
shaochenheng <1010716589@qq.com>
2020-01-08 18:55:49 +0800

    rm a file

--- catch_them/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-        printf("///////////\n");
-        map<int, int>::iterator ite;
-        for ( ite = m.begin(); ite != m.end(); ite ++ )
-        {
-            cout << ite -> first << ' ' << ite -> second<<endl;
-        }
-        printf("///////////\n");
